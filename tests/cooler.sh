13ae09e56ba6b65943093ebcf97b3f7dbb2dc361
Felix Salfelder <felix@salfelder.org>
2022-02-28 00:00:00 +0000

    tidy up before cooler/vtk test

--- tests/cooler.sh
+++ tests/cooler.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -x
 
-${UGSHELL-ugshell} -numRefs 0 -ex ../cooler.lua
+rm -f sol_cooler.pvd sol_cooler_t00*.vtu
 
+${UGSHELL-ugshell} -numRefs 0 -ex ../cooler.lua && \
 tail -n7 sol_cooler.pvd sol_cooler_t00*.vtu
