732f4758c7cd5aa282310c0f4930aa0e254594f4
Dmitriy Logashenko <LogashenkoDL@mail.ru>
2023-01-15 16:49:57 +0300

    Upwind type + Linesearch.

--- elder_bq.lua
+++ elder_bq.lua
@@ -24,6 +24,8 @@ numPreRefs 	= 1
 endTime		= 10 * 365 * 24 * 60 * 60 -- [s] = 10 years
 dt			= 10 * 24 * 60 * 60 -- [s] = 10 days
 
+upwind		= "partial"
+
 vtk_file_name = "my_Elder"
 
 ------------------------------------------------------------------------------------------
@@ -113,7 +115,7 @@ FlowEq:set_flux(DarcyVelocity)
 
 -- transport equation
 TransportEq = ConvectionDiffusionFV1("c", "Inner")
-TransportEq:set_upwind(FullUpwind())
+TransportEq:set_upwind(UpwindFV1(upwind))
 TransportEq:set_mass_scale(Porosity)
 TransportEq:set_velocity(DarcyVelocity)
 TransportEq:set_diffusion(Porosity * MolecularDiffusion)
@@ -136,35 +138,44 @@ domainDisc:add(dirichletBND)
 util.solver.defaults.approxSpace = approxSpace
 solverDesc = {
 	type = "newton",
-
+	
+	lineSearch = {
+		type			= "standard",-- ["standard", "none"]
+		maxSteps		= 8,		-- maximum number of line search steps
+		lambdaStart		= 1,		-- start value for scaling parameter
+		lambdaReduce	= 0.5,		-- reduction factor for scaling parameter
+		acceptBest 		= true,		-- check for best solution if true
+		checkAll		= false		-- check all maxSteps steps if true 
+	},
+	
 	convCheck = {
-		type		= "standard",
-		iterations 	= 30,		-- maximum number of iterations
-		absolute	= 5e-6,		-- absolut value of defect to be reached; usually 1e-7 - 1e-9
-		reduction	= 1e-10,	-- reduction factor of defect to be reached; usually 1e-6 - 1e-8
-		verbose		= true			-- print convergence rates if true
+		type			= "standard",
+		iterations 		= 30,		-- maximum number of iterations
+		absolute		= 5e-6,		-- absolut value of defect to be reached; usually 1e-7 - 1e-9
+		reduction		= 1e-10,	-- reduction factor of defect to be reached; usually 1e-6 - 1e-8
+		verbose			= true		-- print convergence rates if true
 	},
 
 	linSolver = 
 	{
 		type = "bicgstab",
 		precond = {
-	        type            = "gmg",        -- preconditioner ["gmg", "ilu", "ilut", "jac", "gs", "sgs"]
-	        smoother		= "ilu",		-- pre- and postsmoother, only for gmg ["ilu", "ilut", "jac", "gs", "sgs"]
-	        cycle           = "V",          -- gmg-cycle ["V", "F", "W"]
-	        preSmooth       = 2,            -- number presmoothing steps
-	        postSmooth      = 2,            -- number postsmoothing steps
-	        rap             = false,        -- comutes RAP-product instead of assembling if true
-	        baseLevel       = numPreRefs,	-- gmg - baselevel
-	        baseSolver		= "lu",
+	        type		= "gmg",	-- preconditioner ["gmg", "ilu", "ilut", "jac", "gs", "sgs"]
+	        smoother	= "ilu",	-- pre- and postsmoother, only for gmg ["ilu", "ilut", "jac", "gs", "sgs"]
+	        cycle		= "V",		-- gmg-cycle ["V", "F", "W"]
+	        preSmooth	= 2,		-- number presmoothing steps
+	        postSmooth	= 2,		-- number postsmoothing steps
+	        rap			= false,	-- comutes RAP-product instead of assembling if true
+	        baseLevel	= numPreRefs,-- gmg - coarsest level
+	        baseSolver	= "lu",
 		},
 
 		convCheck = {
-	        type            = "standard",
-	        iterations      = 100,          -- number of iterations
-	        absolute        = 1e-8,        -- absolut value of defact to be reached; usually 1e-8 - 1e-10 (must be larger than in newton section)
-	        reduction       = 1e-5,         -- reduction factor of defect to be reached; usually 1e-7 - 1e-8 (must be larger than in newton section)
-	        verbose         = true,         -- print convergence rates if true
+	        type		= "standard",
+	        iterations	= 100,		-- number of iterations
+	        absolute	= 1e-8,		-- absolut value of defact to be reached; usually 1e-8 - 1e-10 (may not be larger than in newton section)
+	        reduction	= 1e-5,		-- reduction factor of defect to be reached
+	        verbose		= true,		-- print convergence rates if true
 		}
 	}
 }
